37c924b27c7eff928a770c637b7fab7f85654975
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-11-17 06:36:53 +0100

    added column class, propreties, getters and setters

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -3,6 +3,9 @@ class Ticket {
     #id;
     #title;
     #description;
+    #column; 
+    #person; 
+
     constructor(title, description) {
         this.#title = title;
         this.#description = description;
@@ -21,6 +24,22 @@ class Ticket {
         return this.#id;
     }
 
+    getColumn() {
+        return this.#column;
+    }   
+
+    setColumn(newColumn) {
+        this.#column = newColumn;
+    }
+
+    getPerson() {
+        return this.#person;
+    }   
+
+    setPerson(newPerson) {
+        this.#person = newPerson;
+    }
+
     setTicketTitle(newTitle) {
         this.#title = newTitle;
     }   
@@ -30,6 +49,11 @@ class Ticket {
 
     renderOnConsole() {
         console.log(`Ticket Info: ${this.getTicket()} - ${this.getTicketDescription()}`);
+        if (this.#person) { 
+            console.log(`Assigned to: ${this.#person.getPerson()}`);
+        } else {
+            console.log(`Assigned to: No one`);
+        }   
     }
 
 
