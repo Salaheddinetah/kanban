5681fda208d537fcad4893419ff5f567a1d28487
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-11-22 13:50:47 +0100

    implement Column class with ticket management methods

--- scripts/column.js
+++ scripts/column.js
@@ -1,3 +1,38 @@
 class Column {
-    
+    #columnName;
+    #tickets;
+
+    constructor(columnName, ticket) {
+        this.#columnName = columnName;
+        this.#tickets = [];
     }
+
+    get columnName() {
+        return this.#columnName;    
+    }
+    get ticket() {
+        return this.#tickets;    
+        }
+
+    canAddTicket() {
+        if (this.#tickets.length > 10) {
+            return false
+        }
+        return true
+    }
+    addTicket(ticket) {
+        if (!this.canAddTicket()) {
+            throw new Error('Cannot add more tickets to this column');
+        }
+        if (ticket.column) {
+            throw new Error('Ticket already belongs to a column');
+        }
+        
+       
+     this.#tickets.push(ticket);
+     return ticket;
+    }
+
+
+// end of class
+}
