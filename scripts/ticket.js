44ec6092b51b680112d3fe1c360f22550b56db43
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-11-24 06:42:13 +0100

    add HTML rendering for Ticket class and enhance styles for better presentation

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -57,6 +57,19 @@ class Ticket {
         console.log(`${this.#id} - ${this.#description} - Assigned to: ${this.#person ? this.#person.person : 'Unassigned'}`);           
 }
 
+renderInHTML(ticketsContainer) {
+    let liHtml = document.createElement('li');
+    liHtml.className = 'ticket';
+
+    liHtml.innerHTML = `
+        <h3 class="ticket-id">${this.#id}</h3>
+        <h3>${this.#title}</h3>
+        <p>${this.#description}</p>
+        <p class="ticket-person">Assigned to: ${this.#person ? this.#person.person : 'Unassigned'}</p>
+    `;
+    ticketsContainer.appendChild(liHtml);
+    }
+
 
 
 
