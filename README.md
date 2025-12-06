7ebfa234a6567e2ed6559d271b732fba70b56a82
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-12-06 19:09:51 +0100

    nodemon installed

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to start:
+
+1. Ga naar de Server directory
+2. npm install
+3. run "npm run start" 
--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,26 @@
 import express from "express";
-
-const app = express();
+import livereload from "connect-livereload";
+import connectLivereload from "connect-livereload";
 
 app.use(express.static("../Client"));
 
+// open livereload high port and start to watch public directory for changes
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'Client', 'Server'));
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 5000");
+// ping browser on Express boot, once browser has reconnected and handshaken
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+const app = express();
+
+// monkey patch every served HTML so they know of changes
+app.use(connectLivereload());
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
 
 
