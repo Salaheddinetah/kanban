d41e2c02a4adb1c13ddbce76aad82f09d7a689c4
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-12-06 23:20:40 +0100

    added dummy chart and tested

--- Client/scripts/index.js
+++ Client/scripts/index.js
@@ -1,6 +1,8 @@
 import { Board } from "./board.js";
 import { getAllPersons } from "./person-database.js";
 import { Ticket } from "./ticket.js";
+import './pie-chart.js';
+
 
 // Aanmaken van een board met 3 kolommen.
 let board = new Board("To do", "Doing", "Done");
--- Client/scripts/pie-chart.js
+++ Client/scripts/pie-chart.js
@@ -2,3 +2,19 @@ import '../node_modules/chart.js/dist/chart.umd.js';
 
 const ctx = document.getElementById('pie-chart');
 
+let statesAndCounts = new Map();
+
+statesAndCounts.set('To Do', 19);
+statesAndCounts.set('In Progress', 9);
+statesAndCounts.set('Done', 12);
+
+let chart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: Array.from(statesAndCounts.keys()),
+        datasets: [{
+            label: 'Task Distribution',
+            data: Array.from(statesAndCounts.values()),
+        }]
+    }
+}); 
