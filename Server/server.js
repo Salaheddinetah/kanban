c90a7e56612b5ada7ee1466255cca32d4f48e8dd
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-12-06 16:31:19 +0100

    added nodemon

--- Server/server.js
+++ Server/server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.static("../Client"));
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 5000");
 });
 
 
